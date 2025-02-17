89d8114054f8304f2d3164c8b37568d140210b2d
Leo <rafaellosibayan36@gmail.com>
2025-02-18 02:54:53 +0800

    Successful implementation of recipe_list template and view

--- ledger/urls.py
+++ ledger/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from .views import index
+from .views import index, recipe_list
 
 urlpatterns = [
     path('', index, name='index'),
+    path('recipe/list', recipe_list, name='recipe_list'),
 ]
 
 app_name = "ledger"
--- ledger/views.py
+++ ledger/views.py
@@ -3,3 +3,7 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse('test')
+    
+def recipe_list(request):
+    return render(request, 'recipe_list.html', {'name': "Leo"})
+    
