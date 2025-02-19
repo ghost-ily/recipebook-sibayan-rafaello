3db001c021541f983064664750eda8b1d063e089
Leo <rafaellosibayan36@gmail.com>
2025-02-19 20:16:25 +0800

    Removed tests

--- ledger/urls.py
+++ ledger/urls.py
@@ -3,7 +3,6 @@ from django.urls import path
 from .views import index, recipe_list, recipe_1, recipe_2
 
 urlpatterns = [
-    path('', index, name='index'),
     path('recipe/list', recipe_list, name='recipe_list'),
     path('recipe/1', recipe_1, name='recipe_1'),
     path('recipe/2', recipe_2, name='recipe_2'),
