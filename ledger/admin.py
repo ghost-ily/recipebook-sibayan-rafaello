10788778fa17c3fbbd3adba36fb74bdb140b46d6
Leo <rafaellosibayan36@gmail.com>
2025-03-06 18:39:15 +0800

    Implemented basic admin panel for Recipe model.

--- ledger/admin.py
+++ ledger/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
+from .models import Recipe, Ingredient, RecipeIngredient
+
+class RecipeAdmin(admin.ModelAdmin):
+    model = Recipe
+    
+    search_fields = ('name', )
+
+admin.site.register(Recipe, RecipeAdmin)
 # Register your models here.
