736f0e4ed2b6be5f0cc21a7199d97e0289efd0b5
Leo <rafaellosibayan36@gmail.com>
2025-03-22 19:55:20 +0800

    Added base.html that extends to current views.

--- ledger/views.py
+++ ledger/views.py
@@ -1,13 +1,16 @@
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
+from django.contrib.auth.mixins import LoginRequiredMixin
 from .models import Recipe
 
 
-class RecipeListView(ListView):
+class RecipeListView(LoginRequiredMixin, ListView):
+    login_url = "http://localhost:8000/accounts/login/"
     model = Recipe
     template_name = 'recipe_list.html'
 
 
-class RecipeDetailView(DetailView):
+class RecipeDetailView(LoginRequiredMixin, DetailView):
+    login_url = "http://localhost:8000/accounts/login/"
     model = Recipe
     template_name = 'recipe_detail.html'
