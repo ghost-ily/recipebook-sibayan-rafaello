2015adc2728a637a59812dd71c06c9dfb9e1851b
Leo <rafaellosibayan36@gmail.com>
2025-03-23 22:32:43 +0800

    Moved registration templates to project-level.

--- ledger/views.py
+++ ledger/views.py
@@ -1,18 +1,19 @@
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.views import PasswordResetView
 from .models import Recipe
 
 
 class RecipeListView(LoginRequiredMixin, ListView):
     model = Recipe
     template_name = 'recipe_list.html'
-    redirect_field_name = "test"
 
 
 class RecipeDetailView(LoginRequiredMixin, DetailView):
     model = Recipe
     template_name = 'recipe_detail.html'
-    redirect_field_name = "test"
    
 
+class PasswordResetView(PasswordResetView):
+    template_name = 'password_reset_form.html'
