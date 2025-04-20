8654d12bb6cda446f128d0255977af7762e677b2
Leo <rafaellosibayan36@gmail.com>
2025-04-21 02:32:16 +0800

    Added RecipeImage model and updated detail view

--- ledger/models.py
+++ ledger/models.py
@@ -60,3 +60,14 @@ class RecipeIngredient(models.Model):
             null=True,
             related_name='ingredients'
         )
+
+
+class RecipeImage(models.Model):
+    image = models.ImageField(upload_to='images/', null=False)
+    description = models.CharField(max_length=255)
+    
+    recipe = models.ForeignKey(
+            Recipe,
+            on_delete=models.CASCADE,
+            related_name='image'
+        )
